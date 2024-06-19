ae8e2a34a15818780888138173772496c8ac0e43
Clint Nathan Calumpad <72256679+kwasonlover@users.noreply.github.com>
2024-06-19 16:48:10 +0800

    feat: 448 pm

--- lab3.ino
+++ lab3.ino
@@ -27,24 +27,44 @@ void loop() {
   waitForInput();
 }
 
+int hasPressed() {
+  for (int i = 0; i < sizeof(btns) / sizeof(btns[0]); i++) {
+    if (digitalRead(btns[i]) == HIGH){
+      return i;
+    }
+  }
+  return sizeof(btns) / sizeof(btns[0]);
+}
+
 void generateSequence() {
+  //ggs na d2 lods
   if (score >= 10) {
     for (int i = 0; i < sizeof(leds) / sizeof(leds[0]); i++) {
       digitalWrite(leds[i], HIGH);
     }
     while (score >= 10) {
-      // hehe
+      // hehe forever loop kasi ggs na
     }
   }
-
+  // generate number here
   for (int k = 0; k < sizeof(sequence) / sizeof(sequence[0]); k++) {
-      auto randomNumber = random(3);
-      sequence[k] = randumNumber;
-    }
+    auto randomNumber = random(3);
+    sequence[k] = randomNumber;
+  }
 }
 
 void displaySequence() {
+  for (int k = 0; k < sizeof(sequence) / sizeof(sequence[0]); k++) {
+    digitalWrite(leds[sequence[k]], HIGH);
+    delay(750);
+    digitalWrite(leds[sequence[k]], LOW);
+    delay(750);
+  }
 }
 
 void waitForInput() {
+  while(!hasPressed()){
+    //loop lang habang hindi pa pumipindot
+  }
 }
+
